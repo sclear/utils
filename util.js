b7ea23d76500516b3fa64286b26155504611e64c
yq <1392227049@qq.com>
2018-10-09 09:38:46 +0800

    小工具 start github

---
+++ util.js
@@ -0,0 +1,10 @@
+//验证电话号码
+function isPhone(value) {
+    return /^0?1[3|4|5|8][0-9]\d{8}$/.test(value);
+}
+
+//验证电话号码
+function isEmail(value) {
+    return /^0?1[3|4|5|8][0-9]\d{8}$/.test(value);
+}
+
