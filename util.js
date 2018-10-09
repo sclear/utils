ecab042fb0a1be4c84dbd5660190b1d8323eac09
yq <1392227049@qq.com>
2018-10-09 09:52:16 +0800

    add

--- util.js
+++ util.js
@@ -3,8 +3,7 @@ function isPhone(value) {
     return /^0?1[3|4|5|8][0-9]\d{8}$/.test(value);
 }
 
-//验证电话号码
+//验证邮箱
 function isEmail(value) {
-    return /^0?1[3|4|5|8][0-9]\d{8}$/.test(value);
+    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(value);
 }
-
