a2f29d920ee5567070e453f5ec5fda280b2df0b1
yq <1392227049@qq.com>
2018-10-09 17:07:11 +0800

    add

--- util.js
+++ util.js
@@ -9,7 +9,15 @@ const tool = {
         return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(value);
     },
 
-
+    //验证身份证号
+    isIdcard(value) {
+        return /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/.test(value);
+    },
+    
+    //验证姓名(中文)
+    isName(value) {
+        return /^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$/.test(value);
+    },
 
     //JSON  string <==> parse
     toJstring(value) {
@@ -22,28 +30,28 @@ const tool = {
 
     //获取当前日期  YYYY - MM - DD
     getDay() {
-        var date = new Date();
-        var seperator1 = "-";
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        var strDate = date.getDate();
+        let date = new Date();
+        let seperator1 = "-";
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let strDate = date.getDate();
         if (month >= 1 && month <= 9) {
             month = "0" + month;
         }
         if (strDate >= 0 && strDate <= 9) {
             strDate = "0" + strDate;
         }
-        var currentdate = year + seperator1 + month + seperator1 + strDate;
+        let currentdate = year + seperator1 + month + seperator1 + strDate;
         return currentdate;
     },
 
     //获取当前时间  H : M       需要秒 传入(任意形参))
     getTime(sec) {
-        var date = new Date();
-        var seperator1 = ":";
-        var hour = date.getHours();
-        var minutes = date.getMinutes();
-        var seconds = date.getSeconds();
+        let date = new Date();
+        let seperator1 = ":";
+        let hour = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
 
         if (sec) return (hour + seperator1 + minutes + seperator1 + seconds)
         else return (hour + seperator1 + minutes)
@@ -57,21 +65,18 @@ const tool = {
         return new Date().getTime()
     },
 
-
-   
-
     // 时间戳   ==>   时间 
     // YYYY-MM-DD  res  => 'y-m-d'
     // h:m         res  => 'h:m' 
     // h:m:s       res  => 'h:m:s' 
     toTime(timestamp, res) {
-        var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
-        var Y = date.getFullYear() + '-';
-        var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-        var D = date.getDate();
-        var h = date.getHours() + ':';
-        var m = date.getMinutes();
-        var s = date.getSeconds();
+        let date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+        let Y = date.getFullYear() + '-';
+        let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+        let D = date.getDate();
+        let h = date.getHours() + ':';
+        let m = date.getMinutes();
+        let s = date.getSeconds();
 
         if (res === 'y-m-d') return (Y + M + D)
         else if (res === 'h:m') return (h + m)
@@ -79,10 +84,8 @@ const tool = {
         else return Y + M + D + ' ' + h + m + ':' + s
     },
 
-    //时间  =>  时间戳
+    //指定时间  =>  时间戳
     toStamap(value) {
         return Date.parse(value);
     }
 }
-
-
